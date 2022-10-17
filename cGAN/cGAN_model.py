1587378f7555bc3355a2c2088948156ac80cf365
Jinwoo <overmind934@naver.com>
2022-10-17 17:34:37 +0900

    model 구성 수정

--- cGAN/cGAN_model.py
+++ cGAN/cGAN_model.py
@@ -5,16 +5,17 @@
 
  Author / date : Jinwoo Lee / 2022-10-11
 
-
  < Dimension >
 
   * Generator
 
-    => 기존의 GAN과 동일 하지만, Condition Vector를 noise sampling vector와 concatenation 
+    => latent_input_dim + label embedding vector = 100 + 10 = 110 
 
-       condition vector dim = [ 128, num_class ] 임으로  ( MNIST 라서 num_class = 10 )
+       embedding( 정수 embedding, 원하는 embedding dim) 의 형태로 사용 되며,
 
-       Generator의 입력 차원은 self.input_dim + num_class(10)
+       NLP Task 에서는 정수 embedding = vocab_size 가 되지만,
+
+       MNIST data의 경우 MNIST_class(0~9) 에 해당함으로 10이 된다 
 
 
   * Discriminator 
@@ -25,104 +26,112 @@
 
        입력 dim = ( 1 * 28 * 28 ) + 10 
  
+< 알아두면 좋은 것 >
+
+ - nn.BatchNorm1d( in_Feature, ..)
+
+   => Generator에서 BN을 사용하지 않으면 discriminator's loss가 0으로 수렴 
+   
+      * 구현에서 nn.Batchnorm(out_feat) 인 이유 
+      
+         nn.linear(in_feat, out_feat) 
+
+         nn.BatchNorm1d(out_feat)  => batchnorm1d 입장에서는 linear의 output이 batch의 input 임으로 
+
 
 '''
 
-import torch
+import torch 
 import torch.nn as nn
-from torch.nn.modules.activation import LeakyReLU
-from torch.nn.modules.batchnorm import BatchNorm1d
 
 
-class Generator(nn.Module):
-    
-    def __init__(self, input_dim):
+class Discriminator(nn.Module):
+
+    def __init__(self, img_size, num_class):
 
         super().__init__()
 
-        self.input_dim = input_dim
+        # nn.Embedding(정수 임베딩 범위, 사용자 정의 embedding dim)
+        self.label_embedding = nn.Embedding(num_class, 10)
+
+        self.model = nn.Sequential(
+
+            nn.Linear( (img_size**2)+ num_class, 512),
+            nn.LeakyReLU(0.2, inplace = True ),
+            
+            nn.Linear(512, 512),
+            nn.Dropout(0.4),
+            nn.LeakyReLU(0.2, inplace = True ),
+            
+            nn.Linear(512, 512),
+            nn.Dropout(0.4),
+            nn.LeakyReLU(0.2, inplace = True ),
+            
+            nn.Linear(512, 1)
+            
+            )
+
+    def forward(self, data, label):
+        
+        label_vector = self.label_embedding(label)
+
+        data = data.view(data.shape[0] , -1 )
+
+        data = torch.cat( (data, label_vector), dim = 1 )
+
+        output = self.model(data)
+
+        return output
+
+
+class Generator(nn.Module):
+
+    def __init__(self, len_latent, num_class):
+        
+        super().__init__()
+
+        self.label_embedding = nn.Embedding(num_class, 10)
+
+        # block 
+        def g_block(input_dim, output_dim, normalize = True):
+
+            layer = [ nn.Linear(input_dim, output_dim ) ]
+
+            if normalize : 
+                # batchnorm1d ( num_feattures, eps, momentum)
+                # eps      : a value added to the denominator for numerical stability
+                # momentum : the value used for the running_mean and running_var computation
+                layer.append( nn.BatchNorm1d(output_dim, momentum = 0.8) )
+
+            layer.append( nn.LeakyReLU(0.2 , inplace = True))
+
+
+            return layer
 
 
         self.model = nn.Sequential(
 
-            nn.Linear(self.input_dim, 128),
-            # batchnorm1d ( num_feattures, eps, momentum)
-            # eps      : a value added to the denominator for numerical stability
-            # momentum : the value used for the running_mean and running_var computation
-            nn.BatchNorm1d(128, momentum = 0.8),
-            
-            # LeakyRelu(negative slope, inplace)  
-            # in_place : 변수를 덮어씀으로써 계산 복잡도 감소
-            nn.LeakyReLU(0.2 , inplace= True),
-
-            nn.Linear(128,256),
-            nn.BatchNorm1d(256, 0.8),
-            nn.LeakyReLU(0.2 , inplace= True),
-
-            nn.Linear(256,512),
-            nn.BatchNorm1d(512, 0.8),
-            nn.LeakyReLU(0.2 , inplace= True),
-
-            nn.Linear(512,1024),
-            nn.BatchNorm1d(1024, 0.8),
-            nn.LeakyReLU(0.2 , inplace= True),
-
-            nn.Linear(1024, 1 * 28 * 28 ),
+            *g_block( len_latent + num_class, 128, normalize = False ),
+            *g_block(128, 256),
+            *g_block(256, 512),
+            *g_block(512, 1024),
+            nn.Linear(1024, 1*28*28),
             nn.Tanh()
 
             )
 
 
-    def forward(self, x):
+    def forward(self, data, label):
         
-        # init x_dim = [batch, latent_input_dim] = [128,100 + 10 ]
-        
-        _batch = x.shape[0] 
-
-        x = self.model(x)
-
-        gener_output = x.view(_batch, 1, 28, 28)
-
-        return gener_output
+        label_vector = self.label_embedding(label)
 
 
-class Discriminator(nn.Module):
-    
+        data = torch.cat( (data, label_vector), dim = 1 )
 
-    def __init__(self):
-        
-        super().__init__()
+        output = self.model(data)
 
-
-        self.model = nn.Sequential(
-
-            nn.Linear((1 * 28 * 28 )+ 10, 1024),
-            nn.LeakyReLU(0.2 , inplace = True ),
-
-            nn.Linear(1024, 512),
-            nn.LeakyReLU(0.2 , inplace = True ),
-
-            nn.Linear(512,256),
-            nn.LeakyReLU(0.2 , inplace = True ),
-
-            nn.Linear(256,1),
-            nn.Sigmoid()
-            
-            )
+        output = output.view(data.shape[0], 1, 28, 28)
 
 
-    def forward(self, x, c):
-
-        # x init dim = [128,1,28,28]
-        # c init dim = [128, 10]
-        _batch = x.shape[0]
-
-        # flatten_x = [128, 784]
-        flatten_x = x.view(_batch, -1)
-
-        # x + conditional vector
-        conditional_x = torch.cat( (flatten_x, c), dim = 1)
-
-        dis_output = self.model(conditional_x)
-
-        return dis_output
+        return output
+ 
